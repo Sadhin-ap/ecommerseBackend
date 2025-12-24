39bc8a4634630328601f0f9f5c51d7cf996e232a
sadhin <sadhin.x7@gmail.com>
2025-12-24 11:55:44 +0530

    closing

--- app/controllers/auth_controller.py
+++ app/controllers/auth_controller.py
@@ -5,6 +5,7 @@ from app.database import get_db
 from app.models.user import User
 from app.schemas.auth import LoginSchema
 from app.core.security import verify_password
+from app.core.jwt import create_access_token
 
 router =APIRouter(prefix="/auth",tags=["Auth"])
 
@@ -14,10 +15,13 @@ def login(data: LoginSchema, db: Session = Depends(get_db)):
     user = db.query(User).filter(User.email == data.email).first()
 
     if not user:
-        raise HTTPException(status_code=400, detail="Invalid credentials")
+        raise HTTPException(status_code=401, detail="Invalid credentials")
 
     if not verify_password(data.password, user.password):
-        raise HTTPException(status_code=400, detail="Invalid credentials")
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    
+    token = create_access_token({"sub": str(user.id)})
+
 
     return {
         "message": "Login successful",
