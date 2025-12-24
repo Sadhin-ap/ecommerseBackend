fbd286a6982f328a58f2676a1fbc9a2b0c12079d
sadhin <sadhin.x7@gmail.com>
2025-12-24 10:50:06 +0530

    password Hashing for backend secure

---
+++ app/controllers/auth_controller.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter,Depends,HTTPException
+from sqlalchemy.orm import Session
+
+from app.database import get_db
+from app.models.user import User
+from app.schemas.auth import LoginSchema
+from app.core.security import verify_password
+
+router =APIRouter(prefix="/auth",tags=["Auth"])
+
+@router.post("/login")
+def login(data: LoginSchema, db: Session = Depends(get_db)):
+
+    user = db.query(User).filter(User.email == data.email).first()
+
+    if not user:
+        raise HTTPException(status_code=400, detail="Invalid credentials")
+
+    if not verify_password(data.password, user.password):
+        raise HTTPException(status_code=400, detail="Invalid credentials")
+
+    return {
+        "message": "Login successful",
+        "user_id": user.id
+    }
