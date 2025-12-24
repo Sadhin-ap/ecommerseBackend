ced7e76a55497d7984b1789267ef4da95440b713
sadhin <sadhin.x7@gmail.com>
2025-12-24 10:20:14 +0530

    created get users , create user and get user

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from app.database import Base, engine
 from app.models import User
+from app.controllers.user_controller import router as user_router
 
 app = FastAPI(title="E-commerce API")
-
+app.include_router(user_router)
 Base.metadata.create_all(bind=engine)
 
 # @app.get("/")
@@ -13,13 +14,13 @@ Base.metadata.create_all(bind=engine)
 from app.schemas.user import UserRead
 
 
-@app.get("/test-schema")
-def test_schema():
-    fake_user = UserRead(
-        id=1,
-        name="Sadhin",
-        email="sadhin@example.com",
-        is_active=True,
-        is_admin=False
-    )
-    return fake_user
+# @app.get("/test-schema")
+# def test_schema():
+#     fake_user = UserRead(
+#         id=1,
+#         name="Sadhin",
+#         email="sadhin@example.com",
+#         is_active=True,
+#         is_admin=False
+#     )
+#     return fake_user
